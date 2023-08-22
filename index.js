5b9538a30c835a9f78aad0ff47bd5b38ded4e8dd
khoanguyen23 <khoahoang151@gmail.com>
2023-08-22 11:44:29 +0700

    [add] vendor prefix

--- index.js
+++ index.js
@@ -12,14 +12,4 @@ window.addEventListener("scroll", function() {
 
   
 
-// window.addEventListener("scroll", function() {
-//     var navbar = document.querySelector(".navbar");
-//     var stickyClass = "sticky";
-  
-//     if (window.scrollY > 0) { // Nếu scrollY > 0, thì thêm lớp "sticky" vào navbar
-//       navbar.classList.add(stickyClass);
-//     } else {
-//       navbar.classList.remove(stickyClass);
-//     }
-//   });
   
